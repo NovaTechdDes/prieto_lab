6bb172754f0c6829977957f5750173114ccda314
Pelusa-not <agustinlorenzatto@outlook.com>
2026-05-06 19:18:54 -0300

    Segundo commit

--- backend_prieto/src/services/saldos.services.ts
+++ backend_prieto/src/services/saldos.services.ts
@@ -1,9 +1,20 @@
 import { pool } from "../config/db";
 
-export async function obtenerSaldos() {
+export async function obtenerSaldos(nombre?: string) {
   try {
-    const result = await pool.query(
-      `select odontologos.id_odontologo, odontologos.nom_odontologo, cta_cte.paciente, sum(cta_cte.debe - cta_cte.haber) as saldo  from odontologos inner join cta_cte on odontologos.id_odontologo = cta_cte.id_odontologo group by odontologos.id_odontologo, odontologos.nom_odontologo, cta_cte.paciente having sum(cta_cte.debe - cta_cte.haber) <> 0.00 order by odontologos.nom_odontologo asc`,
+    const request = pool.request();
+    if (nombre) {
+      request.input("nombre", nombre);
+    }
+
+    const result = await request.query(
+      `select odontologos.id_odontologo, odontologos.nom_odontologo, cta_cte.paciente, sum(cta_cte.debe - cta_cte.haber) as saldo
+        from odontologos 
+        inner join cta_cte on odontologos.id_odontologo = cta_cte.id_odontologo 
+        ${nombre ? "WHERE LOWER(odontologos.nom_odontologo) LIKE LOWER('%' + @nombre + '%')" : ""}
+        group by odontologos.id_odontologo, odontologos.nom_odontologo, cta_cte.paciente 
+        having sum(cta_cte.debe - cta_cte.haber) <> 0.00 
+        order by odontologos.nom_odontologo asc`,
     );
 
     return result.recordset;
